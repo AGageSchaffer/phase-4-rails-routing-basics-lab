04a573863c1966a85b0e7aa01bd638f7b867806a
AustinSchaffer <austinschaffer@gmail.com>
2023-02-21 11:27:00 -0600

    commit tree f4c2f730393aa840b06ba7c85d96a6a1862ea1df

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
 
+  def self.all_grades
+      Student.all.sort {
+        |a, b| b.grade <=> a.grade
+      }
+  end
+
+  def self.highest_grade
+      self.all_grades[0]
+  end
+
   def to_s
     "#{first_name} #{last_name}"
   end
